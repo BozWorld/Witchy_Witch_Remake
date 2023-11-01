f880b21a6f050d99de9d0b201a2910f95333c60d
Moeva <mondohamoeva@hotmail.fr>
2023-11-01 17:38:47 +0100

    {issue with physics}

--- player/PlayerMovement.gd
+++ player/PlayerMovement.gd
@@ -1,1 +1,20 @@
 extends CharacterBody2D
+
+var speed = 25000
+var greeseMeter = 0
+
+signal _greeseScore(gr)
+
+
+func _process(delta):
+	_greeseScore.emit(greeseMeter)
+	
+func _physics_process(delta):	
+	var direction = Input.get_vector("left","right","up","down")
+	velocity = direction * speed * delta
+	print(direction)
+	move_and_slide()
+
+
+func _on_child_entered_tree(GreeseCll):
+	pass # Replace with function body.
---
+++ prefab/LaserBehaviour.gd
@@ -0,0 +1,5 @@
+extends Area2D
+
+
+func _self_destruct():
+	queue_free()
